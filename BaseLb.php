7089ae434bee7f4ba4c517e7f6f2058b4e4a1251
Gogs <gogitservice@gmail.com>
2015-11-06 11:47:09 +0800

    创建composer包

---
+++ BaseLb.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luoxiaojun
+ * Date: 15/11/6
+ * Time: 上午11:26
+ * Lb framework base bootstrap file
+ */
+
+namespace Lb;
+
+class BaseLb
+{
+
+}
---
+++ Lb.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luoxiaojun
+ * Date: 15/11/6
+ * Time: 上午11:31
+ * Lb framework bootstrap file
+ */
+
+namespace Lb;
+
+class Lb extends \Lb\BaseLb
+{
+    public function run()
+    {
+        echo 'Lb Frameword Test';
+    }
+}
