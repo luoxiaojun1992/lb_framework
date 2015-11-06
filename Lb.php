341f7f5b9607c8053e216af1647c5bd06ed03899
Gogs <gogitservice@gmail.com>
2015-11-06 13:06:24 +0800

    修改bootstrap

--- Lb.php
+++ Lb.php
@@ -11,8 +11,37 @@ namespace lb;
 
 class Lb extends \lb\BaseLb
 {
+    public $config;
+    protected $is_single = false;
+    protected static $app;
+
+    public function __construct($is_single = false)
+    {
+        if ($is_single) {
+            $this->is_single = $is_single;
+        }
+
+        // Init Config
+        if (defined('CONFIG_FILE') && file_exists(CONFIG_FILE)) {
+            $this->config = include_once(CONFIG_FILE);
+        }
+    }
+
+    // Singleton App
+    public static function app()
+    {
+        if (self::$app instanceof self) {
+            return self::$app;
+        } else {
+            return (self::$app = new self(true));
+        }
+    }
+
+    // Start App
     public function run()
     {
-        echo 'Lb Frameword Test';
+        if (!$this->is_single) {
+            echo Lb::app()->config['name'];
+        }
     }
 }
