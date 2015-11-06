b0c87c838934ac28712176eacadb3fe00f2546bd
Gogs <gogitservice@gmail.com>
2015-11-06 18:14:23 +0800

    autoload

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -20,14 +20,6 @@ class BaseController
         if (isset(Lb::app()->config['root_dir'])) {
             $root_dir = Lb::app()->config['root_dir'];
 
-            $layouts_dir = $root_dir . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'layouts';
-            if (is_dir($layouts_dir)) {
-                $layout_file_path = $layouts_dir . DIRECTORY_SEPARATOR . $this->layout . '.php';
-                if (file_exists($layout_file_path)) {
-                    include_once($layout_file_path);
-                }
-            }
-
             $views_dir = $root_dir . DIRECTORY_SEPARATOR . 'views';
             if (is_dir($views_dir)) {
                 $view_file_path = $views_dir . DIRECTORY_SEPARATOR . $template_name . '.php';
@@ -35,11 +27,19 @@ class BaseController
                     foreach ($params as $param_name => $param_value) {
                         $$param_name = $param_value;
                     }
-                    if (isset($content)) {
-                        $content = include_once($view_file_path);
-                    } else {
-                        include_once($view_file_path);
+                    $layouts_dir = $root_dir . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'layouts';
+                    if (is_dir($layouts_dir)) {
+                        $layout_file_path = $layouts_dir . DIRECTORY_SEPARATOR . $this->layout . '.php';
+                        if (file_exists($layout_file_path)) {
+                            ob_start();
+                            include_once($view_file_path);
+                            $content = ob_get_contents();
+                            ob_end_clean();
+                            include_once($layout_file_path);
+                            die();
+                        }
                     }
+                    include_once($view_file_path);
                 }
             }
         }
