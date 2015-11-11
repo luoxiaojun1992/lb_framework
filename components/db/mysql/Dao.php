f92694158de9c4d1eb82b12cf26d43b61e0e8168
Your Name <you@example.com>
2015-11-11 11:41:42 +0800

    dao

---
+++ components/db/mysql/Dao.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 224
+ * Date: 2015/11/11
+ * Time: 10:35
+ * Lb framework mysql db dao file
+ */
+
+namespace lb\components\db\mysql;
+
+class Dao
+{
+    protected static $instance = false;
+    protected $_table = '';
+    protected $_fields = [];
+
+    protected $_conditions = [];
+    protected $_orders = [];
+    protected $_limit = '';
+    protected $_group_fields = [];
+
+    protected $_statement = '';
+
+    protected $is_query = false;
+    protected $is_modify = false;
+
+    const SELECT_FROM_SQL_TPL = "SELECT %s FROM %s";
+    const WHERE_SQL_TPL = "WHERE %s";
+    const ORDER_SQL_TPL = "ORDER BY %s";
+    const LIMIT_SQL_TPL = "LIMIT %s";
+    const GROUP_SQL_TPL = "GROUP BY %s";
+
+    public static function component()
+    {
+        if (self::$instance instanceof self) {
+            return self::$instance;
+        } else {
+            return (self::$instance = new self());
+        }
+    }
+
+    public function select($fields)
+    {
+        if (is_array($fields) && $fields) {
+            $this->_fields = $fields;
+        }
+    }
+
+    public function from($table)
+    {
+        $this->is_query = true;
+        $this->is_modify = false;
+        $this->_table = $table;
+    }
+
+    public function where($conditions)
+    {
+        if ($this->_table && is_array($conditions) && $conditions) {
+            $this->_conditions = $conditions;
+        }
+    }
+
+    public function order($orders)
+    {
+        if ($this->_table && is_array($orders) && $orders) {
+            $this->_orders = $orders;
+        }
+    }
+
+    public function limit($limit)
+    {
+        if ($this->_table && $limit) {
+            $this->_limit = $limit;
+        }
+    }
+
+    public function group($group_fields)
+    {
+        if ($this->_table && is_array($group_fields) && $group_fields) {
+            $this->_group_fields = $group_fields;
+        }
+    }
+
+    public function find()
+    {
+        if ($this->is_query) {
+            $query_sql_statement = $this->createStatement();
+            if ($query_sql_statement) {
+                $conn = Connection::component()->conn;
+                if ($conn) {
+                    
+                }
+            }
+        }
+    }
+
+    protected function createStatement()
+    {
+        $statement = '';
+
+        if ($this->is_query) {
+            if ($this->_fields && $this->_table) {
+                $select_from_sql_statement = sprintf(self::SELECT_FROM_SQL_TPL, implode(', ', $this->_fields), $this->_table);
+                $statement .= $select_from_sql_statement;
+
+                // WHERE
+                if ($this->_conditions) {
+                    $conditions = [];
+                    foreach ($this->_conditions as $key => $val) {
+                        if (is_string($val)) {
+                            $conditions[] = implode('=', [$key, '"' . $val . '"']);
+                        } else {
+                            $conditions[] = implode('=', [$key, $val]);
+                        }
+                    }
+                    if ($conditions) {
+                        $condition_statement = implode(' AND ', $conditions);
+                        $where_sql_statement = sprintf(self::WHERE_SQL_TPL, $condition_statement);
+                        $statement .= (' ' . $where_sql_statement);
+                    }
+                }
+
+                // ORDER
+                if ($this->_orders) {
+                    $orders = [];
+                    foreach ($this->_orders as $key => $val) {
+                        $orders[] = implode(' ', [$key, $val]);
+                    }
+                    if ($orders) {
+                        $order_statement = implode(',', $orders);
+                        $order_sql_statement = sprintf(self::ORDER_SQL_TPL, $order_statement);
+                        $statement .= (' ' . $order_sql_statement);
+                    }
+                }
+
+                // LIMIT
+                if ($this->_limit) {
+                    $limit_sql_statement = sprintf(self::LIMIT_SQL_TPL, $this->_limit);
+                    $statement .= (' ' . $limit_sql_statement);
+                }
+
+                // GROUP
+                if ($this->_group_fields) {
+                    $group_sql_statement = sprintf(self::GROUP_SQL_TPL, implode(',', $this->_group_fields));
+                    $statement .= (' ' . $group_sql_statement);
+                }
+            }
+        }
+
+        if ($this->is_modify) {
+
+        }
+
+        return $statement;
+    }
+}
