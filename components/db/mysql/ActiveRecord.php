c7326416ad71fd76b3fad37b8d9a4c8ea62b2b7c
Gogs <gogitservice@gmail.com>
2015-11-15 14:19:40 +0800

    active record

--- components/db/mysql/ActiveRecord.php
+++ components/db/mysql/ActiveRecord.php
@@ -4,4 +4,47 @@
  * User: 224
  * Date: 2015/11/12
  * Time: 17:40
+ * Lb framework mysql db active record file
  */
+
+namespace lb\components\db\mysql;
+
+class ActiveRecord
+{
+    const TABLE_NAME = '';
+    protected $_attributes = [];
+    protected static $_instance = false;
+
+    public function __set($name, $value)
+    {
+        if (array_key_exists($name, $this->_attributes)) {
+            $this->_attributes[$name] = $value;
+        }
+    }
+
+    public function __get($name)
+    {
+        if (array_key_exists($name, $this->_attributes)) {
+            return $this->_attributes[$name];
+        }
+        return false;
+    }
+
+    public function setAttributes($attributes = [])
+    {
+        foreach ($attributes as $attribute_name => $attribute_value) {
+            if (array_key_exists($attribute_name, $this->_attributes)) {
+                $this->_attributes[$attribute_name] = $attribute_value;
+            }
+        }
+    }
+
+    public function model()
+    {
+        if (self::$_instance instanceof self) {
+            return self::$_instance;
+        } else {
+            return (self::$_instance = new self());
+        }
+    }
+}
