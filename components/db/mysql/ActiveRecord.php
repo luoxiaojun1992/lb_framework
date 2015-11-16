48792bbff55c1b7f97b19e56afd92a28cbeb8715
Your Name <you@example.com>
2015-11-16 13:37:31 +0800

    active record

--- components/db/mysql/ActiveRecord.php
+++ components/db/mysql/ActiveRecord.php
@@ -13,27 +13,35 @@ class ActiveRecord
 {
     protected $_primary_key = '';
     protected $_attributes = [];
+    protected $is_single = false;
+    public $is_new_record = true;
 
     public function __set($name, $value)
     {
-        if (array_key_exists($name, $this->_attributes)) {
-            $this->_attributes[$name] = $value;
+        if (!$this->is_single) {
+            if (array_key_exists($name, $this->_attributes)) {
+                $this->_attributes[$name] = $value;
+            }
         }
     }
 
     public function __get($name)
     {
-        if (array_key_exists($name, $this->_attributes)) {
-            return $this->_attributes[$name];
+        if (!$this->is_single) {
+            if (array_key_exists($name, $this->_attributes)) {
+                return $this->_attributes[$name];
+            }
         }
         return false;
     }
 
     public function setAttributes($attributes = [])
     {
-        foreach ($attributes as $attribute_name => $attribute_value) {
-            if (array_key_exists($attribute_name, $this->_attributes)) {
-                $this->_attributes[$attribute_name] = $attribute_value;
+        if (!$this->is_single) {
+            foreach ($attributes as $attribute_name => $attribute_value) {
+                if (array_key_exists($attribute_name, $this->_attributes)) {
+                    $this->_attributes[$attribute_name] = $attribute_value;
+                }
             }
         }
     }
@@ -44,7 +52,11 @@ class ActiveRecord
             if (static::$_instance instanceof static) {
                 return static::$_instance;
             } else {
-                return (static::$_instance = new static());
+                $new_model = new static();
+                $new_model->is_new_record = false;
+                $new_model->is_single = true;
+                static::$_instance = $new_model;
+                return static::$_instance;
             }
         }
         return false;
@@ -52,81 +64,93 @@ class ActiveRecord
 
     public function findAll()
     {
-        $result = Dao::component()->select(['*'])->from(static::TABLE_NAME)->findAll();
-        if ($result) {
-            $models = [];
-            foreach ($result as $attributes) {
-                $model_class = get_class($this);
-                $model = new $model_class();
-                $model->setAttributes($attributes);
-                $models[] = $model;
+        if ($this->is_single) {
+            $result = Dao::component()->select(['*'])->from(static::TABLE_NAME)->findAll();
+            if ($result) {
+                $models = [];
+                foreach ($result as $attributes) {
+                    $model_class = get_class($this);
+                    $model = new $model_class();
+                    $model->setAttributes($attributes);
+                    $model->is_new_record = false;
+                    $models[] = $model;
+                }
+                return $models;
             }
-            return $models;
         }
         return [];
     }
 
     public function findByPk($primary_key)
     {
-        $attributes = Dao::component()
-            ->select(['*'])
-            ->from(static::TABLE_NAME)
-            ->where([$this->_primary_key => $primary_key])
-            ->find();
-        if ($attributes) {
-            $model_class = get_class($this);
-            $model = new $model_class();
-            $model->setAttributes($attributes);
-            return $model;
+        if ($this->is_single) {
+            $attributes = Dao::component()
+                ->select(['*'])
+                ->from(static::TABLE_NAME)
+                ->where([$this->_primary_key => $primary_key])
+                ->find();
+            if ($attributes) {
+                $model_class = get_class($this);
+                $model = new $model_class();
+                $model->setAttributes($attributes);
+                $model->is_new_record = false;
+                return $model;
+            }
         }
         return false;
     }
 
     public function findByConditions($conditions = [], $group_fields = [], $orders = [], $limit = '')
     {
-        $dao = Dao::component()->select(['*'])->from(static::TABLE_NAME);
-        if (is_array($conditions) && $conditions) {
-            $dao->where($conditions);
-        }
-        if (is_array($group_fields) && $group_fields) {
-            $dao->group($group_fields);
-        }
-        if (is_array($orders) && $orders) {
-            $dao->order($orders);
-        }
-        if ($limit) {
-            $dao->limit($limit);
-        }
-        $result = $dao->findAll();
-        if ($result) {
-            $models = [];
-            foreach ($result as $attributes) {
-                $model_class = get_class($this);
-                $model = new $model_class();
-                $model->setAttributes($attributes);
-                $models[] = $model;
+        if ($this->is_single) {
+            $dao = Dao::component()->select(['*'])->from(static::TABLE_NAME);
+            if (is_array($conditions) && $conditions) {
+                $dao->where($conditions);
             }
-            return $models;
+            if (is_array($group_fields) && $group_fields) {
+                $dao->group($group_fields);
+            }
+            if (is_array($orders) && $orders) {
+                $dao->order($orders);
+            }
+            if ($limit) {
+                $dao->limit($limit);
+            }
+            $result = $dao->findAll();
+            if ($result) {
+                $models = [];
+                foreach ($result as $attributes) {
+                    $model_class = get_class($this);
+                    $model = new $model_class();
+                    $model->setAttributes($attributes);
+                    $model->is_new_record = false;
+                    $models[] = $model;
+                }
+                return $models;
+            }
         }
         return [];
     }
 
     public function findBySql($sql)
     {
-        $statement = Dao::component()->prepare($sql, 'slave');
-        if ($statement) {
-            $res = $statement->execute();
-            if ($res) {
-                $result = $statement->fetchAll();
-                if ($result) {
-                    $models = [];
-                    foreach ($result as $attributes) {
-                        $model_class = get_class($this);
-                        $model = new $model_class();
-                        $model->setAttributes($attributes);
-                        $models[] = $model;
+        if ($this->is_single) {
+            $statement = Dao::component()->prepare($sql, 'slave');
+            if ($statement) {
+                $res = $statement->execute();
+                if ($res) {
+                    $result = $statement->fetchAll();
+                    if ($result) {
+                        $models = [];
+                        foreach ($result as $attributes) {
+                            $model_class = get_class($this);
+                            $model = new $model_class();
+                            $model->setAttributes($attributes);
+                            $model->is_new_record = false;
+                            $models[] = $model;
+                        }
+                        return $models;
                     }
-                    return $models;
                 }
             }
         }
@@ -135,14 +159,42 @@ class ActiveRecord
 
     public function getPrimaryKey()
     {
-        if (array_key_exists($this->_primary_key, $this->_attributes)) {
-            return $this->_attributes[$this->_primary_key];
+        if (!$this->is_single) {
+            if (array_key_exists($this->_primary_key, $this->_attributes)) {
+                return $this->_attributes[$this->_primary_key];
+            }
         }
         return 0;
     }
 
     public function getAttributes()
     {
-        return $this->_attributes;
+        if (!$this->is_single) {
+            return $this->_attributes;
+        }
+        return [];
+    }
+
+    protected function beforeSave()
+    {
+
+    }
+
+    protected function afterSave()
+    {
+
+    }
+
+    public function save()
+    {
+        if (!$this->is_single) {
+            $this->beforeSave();
+            if ($this->is_new_record) {
+                $this->is_new_record = false;
+            } else {
+
+            }
+            $this->afterSave();
+        }
     }
 }
