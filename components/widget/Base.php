0fc529b22e54e8b8cc54855aceae7909020802b1
Your Name <you@example.com>
2015-11-20 17:39:25 +0800

    widget

---
+++ components/widget/Base.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 224
+ * Date: 2015/11/20
+ * Time: 17:35
+ * Lb framework base widget file
+ */
+
+namespace lb\components\widget;
+
+class Base
+{
+
+}
