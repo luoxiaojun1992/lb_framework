69261b2c815145cfb4ba0fabff028b57ebffc237
Your Name <you@example.com>
2015-12-14 14:51:25 +0800

    validation

---
+++ components/helpers/ValidationHelper.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 224
+ * Date: 2015/12/14
+ * Time: 14:42
+ * Lb framework validation helper component file
+ */
+
+namespace lb\components\helpers;
+
+use UCSDMath\Validation\Validation;
+
+class ValidationHelper
+{
+    protected static $validator = false;
+
+    public function __clone()
+    {
+        // TODO: Implement __clone() method.
+    }
+
+    protected static function getValidator()
+    {
+        if (static::$validator && static::$validator instanceof Validation) {
+            return static::$validator;
+        } else {
+            return (static::$validator = new Validation());
+        }
+    }
+
+    public static function isUrl($url)
+    {
+        $validator = static::getValidator();
+        return $validator->isValidURL($url);
+    }
+}
