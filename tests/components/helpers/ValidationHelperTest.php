c83a40002ace6994c6378e49ec8b5faf36a38e71
Your Name <you@example.com>
2015-12-15 14:34:30 +0800

    unit test

--- tests/components/helpers/ValidationHelperTest.php
+++ tests/components/helpers/ValidationHelperTest.php
@@ -22,4 +22,29 @@ class ValidationHelperTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue(ValidationHelper::isUrl($httpsUrl));
         $this->assertFalse(ValidationHelper::isUrl($notUrl));
     }
+
+    public function testIsRequired()
+    {
+        $value = 'test value';
+        $this->assertTrue(ValidationHelper::isRequired($value));
+        $this->assertFalse(ValidationHelper::isRequired(''));
+    }
+
+    public function testIsEmail()
+    {
+        $emailAddress = 'admin@aocs.com.cn';
+        $notEmailAddress = 'www.aocs.com.cn';
+        $this->assertTrue(ValidationHelper::isEmail($emailAddress));
+        $this->assertFalse(ValidationHelper::isEmail($notEmailAddress));
+    }
+
+    public function testIsIp()
+    {
+        $ip4 = '192.168.0.1';
+        $ip6 = '::1';
+        $notIp = '192.168.0';
+        $this->assertTrue(ValidationHelper::isIP($ip4));
+        $this->assertTrue(ValidationHelper::isIP($ip6));
+        $this->assertFalse(ValidationHelper::isIP($notIp));
+    }
 }
