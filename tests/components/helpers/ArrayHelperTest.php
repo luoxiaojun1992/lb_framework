61f59bac7b941a023a44f26e76a59bbcefe4d2d6
Your Name <you@example.com>
2015-12-15 13:24:17 +0800

    unit test

--- tests/components/helpers/ArrayHelperTest.php
+++ tests/components/helpers/ArrayHelperTest.php
@@ -25,13 +25,16 @@ class ArrayHelperTest extends \PHPUnit_Framework_TestCase
                 ['name' => 'b', 'age' => 24],
             ],
         ];
+        $notArray = 'name a age 23';
 
         $oneDimensionalArrayDepth = ArrayHelper::array_depth($oneDimensionalArray);
         $twoDimensionalArrayDepth = ArrayHelper::array_depth($twoDimensionalArray);
         $threeDimensionalArrayDepth = ArrayHelper::array_depth($threeDimensionalArray);
+        $notArrayDepth = ArrayHelper::array_depth($notArray);
         $this->assertEquals(1, $oneDimensionalArrayDepth);
         $this->assertEquals(2, $twoDimensionalArrayDepth);
         $this->assertEquals(3, $threeDimensionalArrayDepth);
+        $this->assertEquals(0, $notArrayDepth);
     }
 
     public function testIsMultiArray()
@@ -47,10 +50,12 @@ class ArrayHelperTest extends \PHPUnit_Framework_TestCase
                 ['name' => 'b', 'age' => 24],
             ],
         ];
+        $notArray = 'name a age 23';
 
         $this->assertFalse(ArrayHelper::is_multi_array($oneDimensionalArray));
         $this->assertTrue(ArrayHelper::is_multi_array($twoDimensionalArray));
         $this->assertTrue(ArrayHelper::is_multi_array($threeDimensionalArray));
+        $this->assertFalse(ArrayHelper::is_multi_array($notArray));
     }
 
     public function testToString()
@@ -66,15 +71,19 @@ class ArrayHelperTest extends \PHPUnit_Framework_TestCase
                 ['name' => 'b', 'age' => 24],
             ],
         ];
+        $notArray = 'name a age 23';
         $oneDimensionalArrayExpectedString = print_r($oneDimensionalArray, true);
         $twoDimensionalArrayExpectedString = print_r($twoDimensionalArray, true);
         $threeDimensionalArrayExpectedString = print_r($threeDimensionalArray, true);
+        $notArrayExpectedString = print_r($notArray, true);
 
         $oneDimensionalArrayString = ArrayHelper::toString($oneDimensionalArray);
         $twoDimensionalArrayString = ArrayHelper::toString($twoDimensionalArray);
         $threeDimensionalArrayString = ArrayHelper::toString($threeDimensionalArray);
+        $notArrayString = ArrayHelper::toString($notArray);
         $this->assertEquals($oneDimensionalArrayString, $oneDimensionalArrayExpectedString);
         $this->assertEquals($twoDimensionalArrayString, $twoDimensionalArrayExpectedString);
         $this->assertEquals($threeDimensionalArrayString, $threeDimensionalArrayExpectedString);
+        $this->assertEquals($notArrayString, $notArrayExpectedString);
     }
 }
