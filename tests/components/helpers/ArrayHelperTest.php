862ddae2763cf355b0c645736f59773288980a13
Your Name <you@example.com>
2015-12-15 12:36:34 +0800

    unit test

--- tests/components/helpers/ArrayHelperTest.php
+++ tests/components/helpers/ArrayHelperTest.php
@@ -12,6 +12,47 @@ use lb\components\helpers\ArrayHelper;
 
 class ArrayHelperTest extends \PHPUnit_Framework_TestCase
 {
+    public function testArrayDepth()
+    {
+        $oneDimensionalArray = ['name' => 'a', 'age' => 23];
+        $twoDimensionalArray = [
+            ['name' => 'a', 'age' => 23],
+            ['name' => 'b', 'age' => 24],
+        ];
+        $threeDimensionalArray = [
+            [
+                ['name' => 'a', 'age' => 23],
+                ['name' => 'b', 'age' => 24],
+            ],
+        ];
+
+        $oneDimensionalArrayDepth = ArrayHelper::array_depth($oneDimensionalArray);
+        $twoDimensionalArrayDepth = ArrayHelper::array_depth($twoDimensionalArray);
+        $threeDimensionalArrayDepth = ArrayHelper::array_depth($threeDimensionalArray);
+        $this->assertEquals(1, $oneDimensionalArrayDepth);
+        $this->assertEquals(2, $twoDimensionalArrayDepth);
+        $this->assertEquals(3, $threeDimensionalArrayDepth);
+    }
+
+    public function testIsMultiArray()
+    {
+        $oneDimensionalArray = ['name' => 'a', 'age' => 23];
+        $twoDimensionalArray = [
+            ['name' => 'a', 'age' => 23],
+            ['name' => 'b', 'age' => 24],
+        ];
+        $threeDimensionalArray = [
+            [
+                ['name' => 'a', 'age' => 23],
+                ['name' => 'b', 'age' => 24],
+            ],
+        ];
+
+        $this->assertFalse(ArrayHelper::is_multi_array($oneDimensionalArray));
+        $this->assertTrue(ArrayHelper::is_multi_array($twoDimensionalArray));
+        $this->assertTrue(ArrayHelper::is_multi_array($threeDimensionalArray));
+    }
+
     public function testToString()
     {
         $oneDimensionalArray = ['name' => 'a', 'age' => 23];
