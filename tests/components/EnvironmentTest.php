8cf13a463778971a23b2acbed5f8e1231805c116
Your Name <you@example.com>
2015-12-16 15:32:36 +0800

    unit test

---
+++ tests/components/EnvironmentTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 224
+ * Date: 2015/12/16
+ * Time: 14:50
+ */
+
+namespace lb\tests\components;
+
+class EnvironmentTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetValue()
+    {
+
+    }
+}
