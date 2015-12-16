beab82cf65ab8284e8846af3f03aabd8cb59721e
Gogs <gogitservice@gmail.com>
2015-12-16 23:06:20 +0800

    file helper

--- components/helpers/FileHelper.php
+++ components/helpers/FileHelper.php
@@ -19,4 +19,18 @@ class FileHelper
         }
         return $file_extension_name;
     }
+
+    public static function download($file_path, $file_name)
+    {
+        if (file_exists($file_path)) {
+            $file_size = filesize($file_path);
+            $fp = fopen($file_path, 'r');
+            Header('Content-type: application/octet-stream');
+            Header('Accept-Ranges: bytes');
+            Header('Accept-Length: ' . $file_size);
+            Header('Content-Disposition: attachment; filename=' . $file_name);
+            echo fread($fp, $file_size);
+            fclose($fp);
+        }
+    }
 }
---
+++ components/helpers/ImageHelper.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luoxiaojun
+ * Date: 15/12/16
+ * Time: 下午11:01
+ */
