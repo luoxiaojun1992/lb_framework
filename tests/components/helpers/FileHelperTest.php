ebb2574d758256a192706ca74a0ea77375ed9184
Your Name <you@example.com>
2015-12-22 09:50:00 +0800

    file helper test

--- tests/components/helpers/FileHelperTest.php
+++ tests/components/helpers/FileHelperTest.php
@@ -18,4 +18,20 @@ class FileHelperTest extends \PHPUnit_Framework_TestCase
         $extension = 'php';
         $this->assertEquals($extension, FileHelper::getExtensionName($file_path));
     }
+
+    public function testDownload()
+    {
+        $file_path = __FILE__;
+        $fp = fopen($file_path, 'r');
+        $file_size = filesize($file_path);
+        $expected_content = fread($fp, $file_size);
+        fclose($fp);
+
+        ob_start();
+        FileHelper::download($file_path, 'test');
+        $actual_content = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertEquals($expected_content, $actual_content);
+    }
 }
