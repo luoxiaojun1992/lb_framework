7225961491ed3cf96ff4a7b0ae9687cf93cba6d0
Your Name <you@example.com>
2016-01-04 16:28:13 +0800

    triggers

--- BaseClass.php
+++ BaseClass.php
@@ -11,6 +11,48 @@ namespace lb;
 
 class BaseClass
 {
+    protected static $observers = [];
+
+    public static function setObserver($event, $observer)
+    {
+        static::$observers[] = [
+            'event' => $event,
+            'observer' => $observer,
+        ];
+    }
+
+    public function __call($name, $arguments)
+    {
+        foreach (static::$observers as $observer) {
+            if (isset($observer['event']['static']) && isset($observer['event']['method']) && isset($observer['observer']['type']) && isset($observer['observer']['class']) && isset($observer['observer']['static']) && isset($observer['observer']['method']) && !$observer['event']['static'] && $name == $observer['event']['method']) {
+                $classNameSpace = 'app\\' .$observer['observer']['type'] . 's\\' . $observer['observer']['class'];
+                if (method_exists($classNameSpace, $observer['observer']['method'])) {
+                    if ($observer['observer']['static']) {
+                        $classNameSpace::{$observer['observer']['method']};
+                    } else {
+                        (new $classNameSpace())->{$observer['observer']['method']};
+                    }
+                }
+            }
+        }
+    }
+
+    public function __callStatic($name, $arguments)
+    {
+        foreach (static::$observers as $observer) {
+            if (isset($observer['event']['static']) && isset($observer['event']['method']) && isset($observer['observer']['type']) && isset($observer['observer']['class']) && isset($observer['observer']['static']) && isset($observer['observer']['method']) && $observer['event']['static'] && $name == $observer['event']['method']) {
+                $classNameSpace = 'app\\' .$observer['observer']['type'] . 's\\' . $observer['observer']['class'];
+                if (method_exists($classNameSpace, $observer['observer']['method'])) {
+                    if ($observer['observer']['static']) {
+                        $classNameSpace::{$observer['observer']['method']};
+                    } else {
+                        (new $classNameSpace())->{$observer['observer']['method']};
+                    }
+                }
+            }
+        }
+    }
+
     public static function className()
     {
         return get_called_class();
--- BaseLb.php
+++ BaseLb.php
@@ -879,6 +879,19 @@ class BaseLb extends BaseClass
 
         // CORS
         Security::cors($this->route_info['controller'], $this->route_info['action']);
+
+        // Set Triggers
+        $triggers_config = $config_container->get('triggers');
+        if ($triggers_config) {
+            foreach ($triggers_config as $item) {
+                if (isset($item['observer']) && isset($item['event']['type']) && isset($item['event']['class'])) {
+                    $eventClassNamespace = 'app\\' . $item['event']['type'] . 's\\' . $item['event']['class'];
+                    if (method_exists($eventClassNamespace, 'setObserver')) {
+                        $eventClassNamespace::setObserver($item['event'], $item['observer']);
+                    }
+                }
+            }
+        }
     }
 
     // Start App
