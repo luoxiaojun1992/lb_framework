4abbcde56baa1d13137e8482c919cc47ab7e7209
Your Name <you@example.com>
2016-02-16 17:28:40 +0800

    image helper

--- components/helpers/ImageHelper.php
+++ components/helpers/ImageHelper.php
@@ -26,4 +26,36 @@ class ImageHelper extends BaseClass
         header('Content-type: image/jpeg');
         $builder->output();
     }
+
+    public static function getInfo($file_path)
+    {
+        $info = getimagesize($file_path);
+        if ($info) {
+            list($width, $height, $type) = $info;
+            return [
+                'width' => $width,
+                'height' => $height,
+                'type' => image_type_to_extension($type),
+            ];
+        }
+        return [];
+    }
+
+    public static function getWidth($file_path)
+    {
+        $info = static::getInfo($file_path);
+        return $info ? $info['width'] : false;
+    }
+
+    public static function getHeight($file_path)
+    {
+        $info = static::getInfo($file_path);
+        return $info ? $info['height'] : false;
+    }
+
+    public static function getType($file_path)
+    {
+        $info = static::getInfo($file_path);
+        return $info ? $info['type'] : false;
+    }
 }
