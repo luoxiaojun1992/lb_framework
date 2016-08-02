a103ee1a343c8f8dbb961d651bf8d219fbf6dc33
罗晓俊 <luoxiaojun@angejia.com>
2016-08-02 14:29:49 +0800

    helper functions

--- components/Functions.php
+++ components/Functions.php
@@ -1,5 +1,7 @@
 <?php
 
+use \lb\Lb;
+
 /**
  * No Exception Echo
  */
@@ -9,3 +11,20 @@ function _echo($var)
         echo $var;
     }
 }
+
+/**
+ * Get Configuration By Name
+ */
+function config($config_name)
+{
+    return Lb::app()->getConfigByName($config_name);
+}
+
+/**
+ * Get Environment Value By Name
+ */
+function env($env_name)
+{
+    return Lb::app()->getEnv($env_name);
+}
+
