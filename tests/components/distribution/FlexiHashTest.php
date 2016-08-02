9d3c8e2ac44a4235a3365df7396c9b9f744549fe
Gogs <gogitservice@gmail.com>
2016-08-02 22:02:44 +0800

    flexihash component unit test

--- tests/components/distribution/FlexiHashTest.php
+++ tests/components/distribution/FlexiHashTest.php
@@ -14,19 +14,20 @@ class FlexiHashTest extends \PHPUnit_Framework_TestCase
 {
     protected $flexihash_instance;
     protected $key;
+    protected $servers;
 
     public function setUp()
     {
         parent::setUp();
 
         $this->flexihash_instance = FlexiHash::component();
-        $servers = [
-            8.8.8.9,
-            8.8.8.8,
-            8.8.8.7,
-            8.8.8.6,
-        ]
-        $this->flexihash_instance->addServers($servers);
+        $this->servers = [
+            '8.8.8.9',
+            '8.8.8.8',
+            '8.8.8.7',
+            '8.8.8.6',
+        ];
+        $this->flexihash_instance->addServers($this->servers);
 
         $this->key = 'test_key';
     }
@@ -34,6 +35,6 @@ class FlexiHashTest extends \PHPUnit_Framework_TestCase
     public function testLookUp()
     {
         // todo node id to be verified
-        $this->assertEquals(0, $this->flexihash_instance->lookup($this->key));
+        $this->assertEquals($this->servers[1], $this->flexihash_instance->lookup($this->key));
     }
 }
