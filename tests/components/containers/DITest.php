ba6ce316c0a14ef4f6dbc7eb78bd95ab39eedb6b
Gogs <gogitservice@gmail.com>
2016-08-03 22:58:41 +0800

    DI

---
+++ tests/components/containers/DITest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 224
+ * Date: 2016/8/3
+ * Time: 11:26
+ */
+
+namespace lb\tests\components\containers;
+
+use Mockery as M;
+use lb\components\containers\DI;
+
+class DITest extends \PHPUnit_Framework_TestCase
+{
+    protected $service;
+    protected $container;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->service = M::mock('service');
+        $this->service
+            ->shoudReceive('test')
+            ->with(1)
+            ->andReturn(1);
+        $this->container = DI::component();
+    }
+
+    public function testSetGet()
+    {
+        $this->container->set('service', $this->service);
+        $service = $this->container->get('service');
+        $this->assertEquals(1, $service->test(1);
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        M::close();
+    }
+}
+
