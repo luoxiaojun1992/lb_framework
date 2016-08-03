9e1b8a51a11fb37744e62a72e5bb91100dc601d7
罗晓俊 <luoxiaojun@angejia.com>
2016-08-03 17:31:35 +0800

    DI

--- components/containers/DI.php
+++ components/containers/DI.php
@@ -11,6 +11,10 @@ namespace lb\components\containers;
 
 class DI extends Base
 {
+    const SERVICE_TYPE_CLASS = 'class';
+    const SERVICE_TYPE_ABSTRACT = 'abstract';
+    const SERVICE_TYPE_STRING = 'string';
+
     public function set($service_name, $service_impl)
     {
         $this->{$service_name} = $service_impl;
@@ -20,4 +24,29 @@ class DI extends Base
     {
         return $this->{$service_name};
     }
+
+    protected function get_service_type($service_impl) {
+        if (is_string($service_impl)) {
+            if (interface_exists($service_impl)) {
+                return 'interface';
+            }
+
+            if (class_exists($service_impl)) {
+                $reflectionClass = new ReflectionClass($service_impl);
+
+                if ($reflectionClass->isInstantiable()) {
+                    return static::SERVICE_TYPE_CLASS;
+                }
+
+                if ($reflectionClass->isAbstract()) {
+                    return static::SERVICE_TYPE_ABSTRACT;
+                }
+            }
+
+            return static::SERVICE_TYPE_STRING;
+        } else {
+            return gettype($service_impl);
+        }
+    }
 }
+
