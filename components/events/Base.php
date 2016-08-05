e076e00cce498286be3aedccdfcfa79f09c1a1ba
罗晓俊 <luoxiaojun@angejia.com>
2016-08-05 14:32:22 +0800

    event

---
+++ components/events/Base.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 224
+ * Date: 2016/8/5
+ * Time: 14:26
+ * Lb framework Base Event component file
+ */
+
+use lb\components\events\EventInterface;
+
+class Base implements EventInterface
+{
+    public $data;
+}
+
---
+++ components/events/EventInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 224
+ * Date: 2016/8/5
+ * Time: 14:26
+ * Lb framework Event Interface component file
+ */
+
+interface EventInterface
+{
+    public $data;
+}
+
