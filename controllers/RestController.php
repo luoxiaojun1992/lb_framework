1604a548ff1ec0e4640ade3c35ce60518e1aefab
罗晓俊 <luoxiaojun1992@sina.cn>
2017-02-03 17:40:12 +0800

    rest bugfix

--- controllers/RestController.php
+++ controllers/RestController.php
@@ -49,7 +49,8 @@ class RestController extends BaseController
 
     protected function validRequestMethod()
     {
-        if (strtolower($this->request_method) != strtolower(Lb::app()->getRequestMethod())) {
+        $this->request_method = trim($this->request_method);
+        if ($this->request_method != '*' && strtolower($this->request_method) != strtolower(Lb::app()->getRequestMethod())) {
             $this->response_invalid_request();
         }
     }
