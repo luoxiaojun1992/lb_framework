0c1cdb9a97e71f8986fa47312457d085d49048ce
罗晓俊 <luoxiaojun1992@sina.cn>
2017-02-08 17:28:51 +0800

    queues

---
+++ controllers/console/QueueController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace lb\controllers\console;
+
+class QueueController extends ConsoleController
+{
+    public function push()
+    {
+
+    }
+
+    public function listen()
+    {
+
+    }
+}
