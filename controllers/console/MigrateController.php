aaf1e2a1f2d90acc640301b3ae5cd439879760ea
罗晓俊 <luoxiaojun1992@sina.cn>
2017-02-08 13:25:59 +0800

    refactory

--- controllers/console/MigrateController.php
+++ controllers/console/MigrateController.php
@@ -6,9 +6,34 @@ use lb\Lb;
 
 class MigrateController extends ConsoleController
 {
+    protected function runMigrate()
+    {
+        $app = require Lb::app()->getRootDir() . '/vendor/robmorgan/phinx/app/phinx.php';
+        $app->run();
+    }
+
+    protected function setArgv($method = '')
+    {
+        $argv = $_SERVER['argv'];
+        $_SERVER['argv'] = $method ? ['Lb Migrate', $method] : ['Lb Migrate'];
+        if ($_SERVER['argc'] > 2) {
+            foreach ($argv as $k => $v) {
+                if ($k > 1) {
+                    $_SERVER['argv'][] = $v;
+                }
+            }
+        }
+    }
+
     public function index()
     {
-        $app = require Lb::app()->getRootDir() . '/../vendor/robmorgan/phinx/app/phinx.php';
-        $app->run();
+        $this->setArgv();
+        $this->runMigrate();
+    }
+
+    public function create()
+    {
+        $this->setArgv('create');
+        $this->runMigrate();
     }
 }
