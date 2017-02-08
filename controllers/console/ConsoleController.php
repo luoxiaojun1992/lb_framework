388db1c0e7c74f958c10045a68edf112cfe9cbf1
罗晓俊 <luoxiaojun1992@sina.cn>
2017-02-08 15:52:58 +0800

    update console

--- controllers/console/ConsoleController.php
+++ controllers/console/ConsoleController.php
@@ -3,11 +3,31 @@
 namespace lb\controllers\console;
 
 use lb\controllers\BaseController;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 class ConsoleController extends BaseController
 {
+    /** @var  ConsoleOutput */
+    protected $outputer;
+
     protected function beforeAction()
     {
-        //
+        $this->outputer = new ConsoleOutput();
+    }
+
+    /**
+     * @param $messages
+     */
+    protected function writeln($messages)
+    {
+        $this->outputer->writeln($messages);
+    }
+
+    /**
+     * @param $messages
+     */
+    protected function write($messages)
+    {
+        $this->outputer->write($messages);
     }
 }
