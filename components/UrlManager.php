64bb93b73e36a132aea6c1f9bbd3201d37c58611
罗晓俊 <luoxiaojun1992@sina.cn>
2017-02-20 17:23:26 +0800

    refactory

--- components/UrlManager.php
+++ components/UrlManager.php
@@ -7,55 +7,62 @@ use lb\Lb;
 
 class UrlManager extends BaseClass
 {
+    /**
+     * @param $path
+     * @param bool $replace
+     * @param null $http_response_code
+     */
     public static function redirect($path, $replace = true, $http_response_code = null)
     {
         header("Location: $path", $replace, $http_response_code);
         Lb::app()->stop();
     }
 
+    /**
+     * @param $uri
+     * @param array $query_params
+     * @param bool $ssl
+     * @param int $port
+     * @return string
+     */
     public static function createAbsoluteUrl($uri, $query_params = [], $ssl = false, $port = 80)
     {
-        $uri = static::createRelativeUrl($uri, $query_params);
-        if ($ssl) {
-            $portal = 'https';
-        } else {
-            $portal = 'http';
-        }
-        if ($port == 80) {
-            $http_port = '';
-        } else {
-            $http_port = ':' . $port;
-        }
-        return $portal . '://' . Lb::app()->getHost() . $http_port . $uri;
+        return ($ssl ? 'https' : 'http') . '://' . Lb::app()->getHost() .
+            ($port == 80 ? '' : ':' . $port) . static::createRelativeUrl($uri, $query_params);
     }
 
+    /**
+     * @param $uri
+     * @param array $query_params
+     * @return string
+     */
     public static function createRelativeUrl($uri, $query_params = [])
     {
-        if (strpos($uri, '?') !== false) {
-            $tmpArr = [];
-            foreach ($query_params as $query_param_name => $query_param_value) {
-                if (is_int($query_param_name)) {
-                    $tmpArr[] = $query_param_value;
-                } else {
-                    $tmpArr[] = implode('=', [$query_param_name, $query_param_value]);
-                }
-            }
-            if ($tmpArr) {
-                $uri .= ('&' . implode('&', $tmpArr));
-            }
-        } else {
-            $tmpArr = [];
-            foreach ($query_params as $query_param_name => $query_param_value) {
-                if (is_int($query_param_name)) {
-                    $tmpArr[] = $query_param_value;
-                } else {
-                    $tmpArr[] = implode('=', [$query_param_name, $query_param_value]);
-                }
-            }
-            if ($tmpArr) {
-                $uri .= ('?' . implode('&', $tmpArr));
+        if ($query_params && ($query = static::build_query($query_params))) {
+            if (strpos($uri, '?') !== false) {
+                $uri .= ('&' . $query);
+            } else {
+                $uri .= ('?' . $query);
             }
         }
+
         return $uri;
+    }
+
+    /**
+     * @param $query_params
+     * @return string
+     */
+    public static function build_query($query_params)
+    {
+        $tmpArr = [];
+        foreach ($query_params as $query_param_name => $query_param_value) {
+            if (is_int($query_param_name)) {
+                $tmpArr[] = $query_param_value;
+            } else {
+                $tmpArr[] = implode('=', [$query_param_name, $query_param_value]);
+            }
+        }
+        return implode('&', $tmpArr);
     }
 }
