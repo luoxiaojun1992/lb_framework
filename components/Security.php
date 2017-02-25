44ae9f8f61514c6f8c8b66e478ce8cdcac0d1ed1
Gogs <gogitservice@gmail.com>
2017-02-25 22:04:22 +0800

    refactory

--- components/Security.php
+++ components/Security.php
@@ -13,182 +13,67 @@ class Security extends BaseClass
     protected static $getfilter = "'|(and|or)\\b.+?(>|<|=|in|like)|\\/\\*.+?\\*\\/|<\\s*script\\b|\\bEXEC\\b|UNION.+?SELECT|UPDATE.+?SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE).+?FROM|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)";
     protected static $postfilter = "\\b(and|or)\\b.{1,6}?(=|>|<|\\bin\\b|\\blike\\b)|\\/\\*.+?\\*\\/|<\\s*script\\b|\\bEXEC\\b|UNION.+?SELECT|UPDATE.+?SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE).+?FROM|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)";
     protected static $cookiefilter = "\\b(and|or)\\b.{1,6}?(=|>|<|\\bin\\b|\\blike\\b)|\\/\\*.+?\\*\\/|<\\s*script\\b|\\bEXEC\\b|UNION.+?SELECT|UPDATE.+?SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE).+?FROM|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)";
-    protected static $xssfilters = [
-        "/\\s+/",
-        "/<(\\/?)(script|i?frame|style|html|body|title|link|meta|object|\\?|\\%)([^>]*?)>/isU",
-        "/(<[^>]*)on[a-zA-Z]+\s*=([^>]*>g)/isU",
-    ];
 
     public static function inputFilter(&$params = [])
     {
-        $expected_request_names = [];
-        $html_decode_names = [];
         if ($params) {
             foreach ($params as $request_name => $request_value) {
-                if (!in_array($request_name, $expected_request_names)) {
-                    $value = static::getFilteredInput($request_value);
-                    if (in_array($request_name, $html_decode_names)) {
-                        $value = HtmlHelper::decode($value);
-                    }
-                    $params[$request_name] = $value;
-                }
+                    $params[$request_name] = static::getFilteredInput($request_value);;
             }
         } else {
-            foreach ($_REQUEST as $request_name => $request_value) {
-                if (!in_array($request_name, $expected_request_names)) {
-                    $value = static::getFilteredInput($request_value);
-                    if (in_array($request_name, $html_decode_names)) {
-                        $value = HtmlHelper::decode($value);
-                    }
-                    $_REQUEST[$request_name] = $value;
-                }
-            }
-            foreach ($_GET as $request_name => $request_value) {
-                if (!in_array($request_name, $expected_request_names)) {
-                    $value = static::getFilteredInput($request_value);
-                    if (in_array($request_name, $html_decode_names)) {
-                        $value = HtmlHelper::decode($value);
-                    }
-                    $_GET[$request_name] = $value;
-                }
-            }
-            foreach ($_POST as $request_name => $request_value) {
-                if (!in_array($request_name, $expected_request_names)) {
-                    $value = static::getFilteredInput($request_value);
-                    if (in_array($request_name, $html_decode_names)) {
-                        $value = HtmlHelper::decode($value);
-                    }
-                    $_POST[$request_name] = $value;
-                }
-            }
-            foreach ($_COOKIE as $request_name => $request_value) {
-                if (!in_array($request_name, $expected_request_names)) {
-                    $value = static::getFilteredInput($request_value);
-                    if (in_array($request_name, $html_decode_names)) {
-                        $value = HtmlHelper::decode($value);
-                    }
-                    $_COOKIE[$request_name] = $value;
-                }
-            }
+            $_REQUEST && self::inputFilter($_REQUEST);
+            $_GET && self::inputFilter($_GET);
+            $_POST && self::inputFilter($_POST);
+            $_COOKIE && self::inputFilter($_COOKIE);
         }
     }
 
-    //Remove the exploer'bug XSS
-    protected static function RemoveXSS($val) {
-        // remove all non-printable characters. CR(0a) and LF(0b) and TAB(9) are allowed
-        // this prevents some character re-spacing such as <java\0script>
-        // note that you have to handle splits with \n, \r, and \t later since they *are* allowed in some inputs
-        $val = preg_replace('/([\x00-\x08,\x0b-\x0c,\x0e-\x19])/', '', $val);
-        // straight replacements, the user should never need these since they're normal characters
-        // this prevents like <IMG SRC=@avascript:alert('XSS')>
-        $search = 'abcdefghijklmnopqrstuvwxyz';
-        $search .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $search .= '1234567890!@#$%^&*()';
-        $search .= '~`";:?+/={}[]-_|\'\\';
-        for ($i = 0; $i < strlen($search); ++$i) {
-            // ;? matches the ;, which is optional
-            // 0{0,7} matches any padded zeros, which are optional and go up to 8 chars
-            // @ @ search for the hex values
-            $val = preg_replace('/(&#[xX]0{0,8}'.dechex(ord($search[$i])).';?)/i', $search[$i], $val);
-            // @ @ 0{0,7} matches '0' zero to seven times
-            $val = preg_replace('/(�{0,8}'.ord($search[$i]).';?)/', $search[$i], $val);
-        }
-        // now the only remaining whitespace attacks are \t, \n, and \r
-        $ra1 = array('javascript', 'vbscript', 'expression', 'applet', 'meta', 'xml', 'blink', 'link', 'style', 'script', 'embed', 'object', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'bgsound', 'title', 'base');
-        $ra2 = array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload');
-        $ra = array_merge($ra1, $ra2);
-        $found = true; // keep replacing as long as the previous round replaced something
-        while ($found == true) {
-            $val_before = $val;
-            for ($i = 0; $i < sizeof($ra); ++$i) {
-                $pattern = '/';
-                for ($j = 0; $j < strlen($ra[$i]); ++$j) {
-                    if ($j > 0) {
-                        $pattern .= '(';
-                        $pattern .= '(&#[xX]0{0,8}([9ab]);)';
-                        $pattern .= '|';
-                        $pattern .= '|(�{0,8}([9|10|13]);)';
-                        $pattern .= ')*';
-                    }
-                    $pattern .= $ra[$i][$j];
-                }
-                $pattern .= '/i';
-                $replacement = substr($ra[$i], 0, 2).'<x>'.substr($ra[$i], 2); // add in <> to nerf the tag
-                $val = preg_replace($pattern, $replacement, $val); // filter out the hex tags
-                if ($val_before == $val) {
-                    // no replacements were made, so exit the loop
-                    $found = false;
-                }
-            }
-        }
-        return $val;
-    }
-
+    /**
+     * @param $input_value
+     * @return array|mixed
+     */
     protected static function getFilteredInput($input_value)
     {
-        if (!is_array($input_value)) {
-            foreach (static::$xssfilters as $xssfilter) {
-                if (preg_match($xssfilter, $input_value) == true) {
-                    $input_value = preg_replace($xssfilter, '', $input_value);
-                    $input_value = static::RemoveXSS($input_value);
-                }
-            }
-            $input_value = trim($input_value);
-            $input_value = addslashes($input_value);
-            $input_value = HtmlHelper::encode($input_value);
-        } else {
-            foreach ($input_value as $key => $value) {
-                if (!is_array($value)) {
-                    foreach (static::$xssfilters as $xssfilter) {
-                        if (preg_match($xssfilter, $value) == true) {
-                            $value = preg_replace($xssfilter, '', $value);
-                            $value = static::RemoveXSS($value);
-                        }
-                    }
-                    $value = trim($value);
-                    $value = addslashes($value);
-                    $value = HtmlHelper::encode($value);
-                    $input_value[$key] = $value;
-                }
-            }
-        }
-        $input_value = static::removeSqlInjection($input_value);
-        return $input_value;
-    }
-
-    protected static function filter($input_value, $filter){
         if (is_array($input_value)) {
-            foreach ($input_value as $key => $value) {
-                if (!is_array($value)) {
-                    if (preg_match("/" . $filter . "/is", $value) == true) {
-                        $value = preg_replace("/" . $filter . "/is", '', $value);
-                        $input_value[$key] = $value;
-                    }
-                }
+            foreach ($input_value as $k => $value) {
+                $input_value[$k] = self::getFilteredInput($value);
             }
         } else {
-            if (preg_match("/".$filter."/is", $input_value) == true) {
-                $input_value = preg_replace("/".$filter."/is", '', $input_value);
-            }
+            $input_value = static::removeSqlInjection($input_value);
         }
         return $input_value;
     }
 
+    /**
+     * @param $value
+     * @return array|mixed
+     */
     public static function removeSqlInjection($value)
     {
-        $filter_name = strtolower(Lb::app()->getRequestMethod()) . 'filter';
-        if (property_exists(get_called_class(), $filter_name)) {
-            $filter = static::$$filter_name;
-            $value = static::filter($value, $filter);
+        if (property_exists(
+            get_called_class(),
+            $filter_name = strtolower(Lb::app()->getRequestMethod()) . 'filter'
+        )) {
+            $filter = "/" . static::$$filter_name . "/is";
+            return preg_match($filter, $value) == true ?
+                preg_replace($filter, '', $value) : $value;
         }
         return $value;
     }
 
+    /**
+     * @return string
+     */
     public static function generateCsrfToken()
     {
         return md5(uniqid(rand(), true));
     }
 
+    /**
+     * @param $controller
+     * @param $action
+     * @throws HttpException
+     */
     public static function validCsrfToken($controller, $action)
     {
         $csrf_config = Lb::app()->getCsrfConfig();
@@ -210,6 +95,10 @@ class Security extends BaseClass
         }
     }
 
+    /**
+     * @param $controller
+     * @param $action
+     */
     public static function cors($controller, $action)
     {
         if (isset(Lb::app()->containers['config'])) {
@@ -223,6 +112,10 @@ class Security extends BaseClass
         }
     }
 
+    /**
+     * @param $controller
+     * @param $action
+     */
     public static function x_frame_options($controller, $action)
     {
         if (isset(Lb::app()->containers['config'])) {
@@ -242,6 +135,10 @@ class Security extends BaseClass
         header("X-Frame-Options: DENY");
     }
 
+    /**
+     * @param $controller
+     * @param $action
+     */
     public static function x_xss_protection($controller, $action)
     {
         if (isset(Lb::app()->containers['config'])) {
@@ -261,6 +158,11 @@ class Security extends BaseClass
         header("X-XSS-Protection: 1");
     }
 
+    /**
+     * @param $controller
+     * @param $action
+     * @throws HttpException
+     */
     public static function ipFilter($controller, $action)
     {
         if (isset(Lb::app()->containers['config'])) {
