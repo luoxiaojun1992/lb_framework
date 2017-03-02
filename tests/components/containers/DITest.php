81746b7996d9354dd72240f4bc742a82357da19f
罗晓俊 <luoxiaojun1992@sina.cn>
2017-03-02 14:54:45 +0800

    test refactory

--- tests/components/containers/DITest.php
+++ tests/components/containers/DITest.php
@@ -2,6 +2,7 @@
 
 namespace lb\tests\components\containers;
 
+use lb\components\containers\DI;
 use lb\Lb;
 use lb\tests\BaseTestCase;
 use Mockery as M;
@@ -9,7 +10,9 @@ use Mockery as M;
 class DITest extends BaseTestCase
 {
     protected $service;
+    /** @var  DI */
     protected $container;
+    protected $callFunc;
 
     public function setUp()
     {
@@ -20,14 +23,44 @@ class DITest extends BaseTestCase
             ->shouldReceive('test')
             ->with(1)
             ->andReturn(1);
+
+        $this->callFunc = function () {
+            return 1;
+        };
+
         $this->container = Lb::app()->getDIContainer();
     }
 
     public function testSetGet()
     {
+        //Inject string
+        $this->container->set('string', 'test');
+        $this->assertEquals('test', $this->container->get('string'));
+
+        //Inject integer
+        $this->container->set('integer', 1);
+        $this->assertEquals(1, $this->container->get('integer'));
+
+        //Inject callable
+        $this->container->set('callable', $this->callFunc);
+        $this->assertEquals(1, $this->container->get('callable'));
+
+        //Inject Object
         $this->container->set('service', $this->service);
-        $service = $this->container->get('service');
-        $this->assertEquals(1, $service->test(1));
+        $this->assertEquals(1, $this->container->get('service')->test(1));
+
+        //Inject Class Name
+        $this->container->set('std_class_interface', \stdClass::class);
+        $this->assertEquals(\stdClass::class, get_class($this->container->get('std_class_interface')));
+
+        //Get Class Name
+        $this->assertEquals(\stdClass::class, get_class($this->container->get(\stdClass::class)));
+
+        //Get Callable
+        $this->assertEquals(1, $this->container->get($this->callFunc));
+
+        //Get String
+        $this->assertEquals('test string', $this->container->get('test string'));
     }
 
     public function tearDown()
