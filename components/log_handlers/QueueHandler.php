c366eef63ee4446b232bf47c502a7f440066e7e0
罗晓俊 <luoxiaojun1992@sina.cn>
2017-03-30 11:13:18 +0800

    add queue log handler

---
+++ components/log_handlers/QueueHandler.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace lb\components\log_handlers;
+
+use Monolog\Logger;
+use Monolog\Handler\AbstractProcessingHandler;
+
+class QueueHandler extends AbstractProcessingHandler
+{
+
+}
