c66e3e4cf1db5006311da9bbf27f67dace3e84d9
罗晓俊 <luoxiaojun1992@sina.cn>
2017-04-01 19:02:10 +0800

    intergreate swoole

--- controllers/console/SwooleController.php
+++ controllers/console/SwooleController.php
@@ -11,7 +11,7 @@ class SwooleController
         $server = new HttpServer('127.0.0.1', 9501);
 
         $server->on('Request', function ($request, $response) {
-            $response->end($request->server('request_uri'));
+            $response->end($request->server['request_uri']);
         });
 
         $server->start();
