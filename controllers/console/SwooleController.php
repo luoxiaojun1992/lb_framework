60e54a6aac120e0d1e422ecb72ffbd13391fe062
罗晓俊 <luoxiaojun1992@sina.cn>
2017-04-01 18:51:49 +0800

    intergreate swoole

--- controllers/console/SwooleController.php
+++ controllers/console/SwooleController.php
@@ -2,14 +2,16 @@
 
 namespace lb\controllers\console;
 
+use \Swoole\Http\Server as HttpServer;
+
 class SwooleController
 {
     public function test()
     {
-        $server = new Swoole\Http\Server('127.0.0.1', 9501);
+        $server = new HttpServer('127.0.0.1', 9501);
 
         $server->on('Request', function ($request, $response) {
-            $response->end(" swoole response is ok");
+            $response->end($request->header('REQUEST_URI'));
         });
 
         $server->start();
