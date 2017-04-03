017397ed5f783ae459d4f8b330be4a327f089d91
Gogs <gogitservice@gmail.com>
2017-04-03 17:03:59 +0800

    intergrate swoole

--- controllers/console/SwooleController.php
+++ controllers/console/SwooleController.php
@@ -12,20 +12,47 @@ class SwooleController
         $server = new HttpServer('127.0.0.1', 9501);
 
         $server->on('Request', function ($request, $response) {
-            $_GET = $request->get;
-            $_POST = $request->post;
-            $_COOKIE = $request->cookie;
-            foreach ($request->server as $item => $value) {
-                $_SERVER[strtoupper($item)] = $value;
-            }
+
+            $this->setRequestParams($request);
 
             Lb::app()->setRouteInfo(true);
 
             Lb::app()->initWebApp();
 
             $response->end(Lb::app()->getHttpResponse());
+
         });
 
         $server->start();
+    }
+
+    protected function setRequestParams($request)
+    {
+        $_GET = $request->get;
+        $_POST = $request->post;
+
+        foreach ($request->get as $item => $value) {
+            $_REQUEST[$item] = $value;
+        }
+
+        foreach ($request->post as $item => $value) {
+            $_REQUEST[$item] = $value;
+        }
+
+        $_COOKIE = $request->cookie;
+
+        foreach ($request->server as $item => $value) {
+            $_SERVER[strtoupper($item)] = $value;
+        }
+
+        foreach ($request->header as $item => $value) {
+            $_SERVER[strtoupper('HTTP_' . $item)] = $value;
+        }
+
+        foreach ($request->files as $item => $value) {
+            $_FILES[$item] = $value;
+        }
+
+        $GLOBALS['HTTP_RAW_POST_DATA'] = $request->rawContent();
     }
 }
