49b31a629885f63e07371505a9c999afd6a67c68
罗晓俊 <luoxiaojun1992@sina.cn>
2017-05-02 16:04:24 +0800

    bugfix

--- components/traits/PcntlSignal.php
+++ components/traits/PcntlSignal.php
@@ -6,7 +6,6 @@ trait PcntlSignal
 {
     public function listenPcntlSignals(Array $signals, $callback)
     {
-        declare(ticks=1);
         foreach ($signals as $signal) {
             pcntl_signal($signal, $callback);
         }
