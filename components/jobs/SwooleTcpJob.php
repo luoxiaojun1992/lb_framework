dd1307e6644c81bd0d7fc9939cef7ce17a0c15f6
罗晓俊 <luoxiaojun1992@sina.cn>
2017-05-02 17:33:23 +0800

    bugfix

--- components/jobs/SwooleTcpJob.php
+++ components/jobs/SwooleTcpJob.php
@@ -2,12 +2,14 @@
 
 namespace lb\components\jobs;
 
+use lb\components\helpers\JsonHelper;
+
 class SwooleTcpJob extends BaseJob
 {
-    public function hanlder($data, $fromId)
+    public function hanlder($data)
     {
         parent::hanlder($data);
 
-        return 'Swoole Tcp Test Success';
+        return 'Swoole Tcp Test Success, data:' . JsonHelper::encode($this->getData());
     }
 }
