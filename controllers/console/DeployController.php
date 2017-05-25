1d7443a3e5a167464ada0159dac32d142a6b1cd8
罗晓俊 <luoxiaojun1992@sina.cn>
2017-05-25 14:01:16 +0800

    refactory

--- controllers/console/DeployController.php
+++ controllers/console/DeployController.php
@@ -13,6 +13,29 @@ class DeployController extends ConsoleController
         $rootDir = Lb::app()->getRootDir();
         $this->changeDir($rootDir);
 
+        $gitBranch = 'master';
+        $isUpdate = false;
+
+        $argv = \Console_Getopt::readPHPArgv();
+        $opts = \Console_Getopt::getopt(array_slice($argv, 2, count($argv) - 2), 'g::c::', null, true);
+        if (!empty($opts[0]) && is_array($opts[0])) {
+            foreach ($opts[0] as $val) {
+                if (!empty($val[0]) && !empty($val[1]) && is_string($val[0]) && is_string($val[1])) {
+                    switch ($val[0]) {
+                        case 'g':
+                            $gitBranch = $val[1];
+                            break;
+                        case 'c':
+                            $isUpdate = $val[1] == 'update';
+                            break;
+                    }
+                }
+            }
+        }
+
+        $this->gitPull($gitBranch);
+        $this->composerInstall($isUpdate);
+
         $this->writeln('Finished.');
     }
 
