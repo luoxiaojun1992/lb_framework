167765178146bb99ead6a157f0bc4311077f913c
罗晓俊 <luoxiaojun1992@sina.cn>
2017-06-09 14:48:43 +0800

    add console

--- controllers/console/HelpController.php
+++ controllers/console/HelpController.php
@@ -1,13 +1,35 @@
 <?php
 
-namespace lb\components\containers\console;
+namespace lb\controllers\console;
 
-use lb\controllers\console\ConsoleController;
+use lb\Lb;
 
 class HelpController extends ConsoleController
 {
     public function index()
     {
+        $files = array_merge(
+            $this->readConsoleControllers(Lb::app()->getRootDir() . '/controllers/console'),
+            $this->readConsoleControllers(Lb::app()->getRootDir() . '/vendor/lbsoft/lb_framework/controllers/console')
+        );
+        var_dump($files);
         $this->writeln('Building...');
+    }
+
+    protected function readConsoleControllers($dir)
+    {
+        $files = [];
+        $fd = opendir($dir);
+        while ($file = readdir($fd)) {
+            if (!in_array($file, ['.', '..'])) {
+                if (!is_dir($file)) {
+                    $files[] = $dir . '/' . $file;
+                } else {
+                    $files = array_merge($files, $this->readConsoleControllers($file));
+                }
+            }
+        }
+        closedir($fd);
+        return $files;
     }
 }
