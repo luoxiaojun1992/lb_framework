94fcea587ef21ec1f4c25a4c5b012974379c854b
Gogs <gogitservice@gmail.com>
2017-06-13 00:17:16 +0800

    console help cache

--- controllers/console/HelpController.php
+++ controllers/console/HelpController.php
@@ -3,6 +3,7 @@
 namespace lb\controllers\console;
 
 use lb\components\helpers\AlgoHelper;
+use lb\components\helpers\JsonHelper;
 use lb\Lb;
 use PhpParser\Comment\Doc;
 use PhpParser\Node\Stmt\Class_;
@@ -27,28 +28,37 @@ class HelpController extends ConsoleController
 
         foreach ($files as $file) {
             $parser = (new ParserFactory())->create(ParserFactory::PREFER_PHP7);
-            $stmts = $parser->parse(file_get_contents($file));
-            foreach ($stmts[0]->stmts as $stmt) {
-                if ($stmt instanceof Class_) {
-                    $className = str_replace('controller', '', strtolower($stmt->name));
-                    $children = [];
-                    foreach ($stmt->stmts as $stmt2) {
-                        if ($stmt2 instanceof ClassMethod && $stmt2->flags == 1) {
-                            $methodName = $stmt2->name;
-                            $attributes = $stmt2->getAttributes();
-                            if (isset($attributes['comments'])) {
-                                /** @var Doc $comment */
-                                $comment = $attributes['comments'][0];
-                                $commentText = $comment->getReformattedText();
-                                $commets = explode(PHP_EOL, $commentText);
-                                $methodName .= (' "' . trim(str_replace('*', '', $commets[1])) . '"');
+            $fileContent = file_get_contents($file);
+            $cacheKey = md5($fileContent);
+            if ($cacheTree = Lb::app()->getCache($cacheKey)) {
+                $tree = array_merge($tree, JsonHelper::decode($cacheKey));
+            } else {
+                $tempTree = [];
+                $stmts = $parser->parse($fileContent);
+                foreach ($stmts[0]->stmts as $stmt) {
+                    if ($stmt instanceof Class_) {
+                        $className = str_replace('controller', '', strtolower($stmt->name));
+                        $children = [];
+                        foreach ($stmt->stmts as $stmt2) {
+                            if ($stmt2 instanceof ClassMethod && $stmt2->flags == 1) {
+                                $methodName = $stmt2->name;
+                                $attributes = $stmt2->getAttributes();
+                                if (isset($attributes['comments'])) {
+                                    /** @var Doc $comment */
+                                    $comment = $attributes['comments'][0];
+                                    $commentText = $comment->getReformattedText();
+                                    $commets = explode(PHP_EOL, $commentText);
+                                    $methodName .= (' "' . trim(str_replace('*', '', $commets[1])) . '"');
+                                }
+                                $children[] = ['value' => $methodName, 'children' => []];
                             }
-                            $children[] = ['value' => $methodName, 'children' => []];
                         }
+                        $tempTree[] = ['value' => $className, 'children' => $children];
+                        break;
                     }
-                    $tree[] = ['value' => $className, 'children' => $children];
-                    break;
                 }
+                $tree = array_merge($tree, $tempTree);
+                Lb::app()->setCache($cacheKey, JsonHelper::encode($tempTree));
             }
         }
 
