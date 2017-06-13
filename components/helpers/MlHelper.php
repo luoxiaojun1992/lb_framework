d3d15ddda80b762f50a5f794c9ed9c7af4ab3de1
罗晓俊 <luoxiaojun1992@sina.cn>
2017-06-13 18:13:19 +0800

    add svc

--- components/helpers/MlHelper.php
+++ components/helpers/MlHelper.php
@@ -6,6 +6,8 @@ use lb\BaseClass;
 use Phpml\Classification\KNearestNeighbors;
 use Phpml\Dataset\CsvDataset;
 use Phpml\Preprocessing\Normalizer;
+use Phpml\Classification\SVC;
+use Phpml\SupportVectorMachine\Kernel;
 
 class MlHelper extends BaseClass
 {
@@ -19,6 +21,21 @@ class MlHelper extends BaseClass
         $normalizer->transform($samples);
 
         $classifier = new KNearestNeighbors();
+        $classifier->train($samples, $dataSet->getTargets());
+
+        return $classifier;
+    }
+
+    public static function svc($dataSet, $featureNum, $ignoreHeader = false)
+    {
+        $dataSet = new CsvDataset($dataSet, $featureNum, $ignoreHeader);
+
+        $samples = $dataSet->getSamples();
+        $normalizer = new Normalizer();
+        $normalizer->fit($samples);
+        $normalizer->transform($samples);
+
+        $classifier = new SVC(Kernel::LINEAR, $cost = 1000);
         $classifier->train($samples, $dataSet->getTargets());
 
         return $classifier;
