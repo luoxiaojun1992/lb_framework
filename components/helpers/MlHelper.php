fee5ce0d5e84f3384ca6097efcd533a4d54ef5b1
罗晓俊 <luoxiaojun1992@sina.cn>
2017-06-13 18:05:11 +0800

    modify mlhelper

--- components/helpers/MlHelper.php
+++ components/helpers/MlHelper.php
@@ -3,18 +3,24 @@
 namespace lb\components\helpers;
 
 use lb\BaseClass;
+use Phpml\Classification\KNearestNeighbors;
 use Phpml\Dataset\CsvDataset;
-use Phpml\NeuralNetwork\Network\MultilayerPerceptron;
-use Phpml\NeuralNetwork\Training\Backpropagation;
+use Phpml\Preprocessing\Normalizer;
 
 class MlHelper extends BaseClass
 {
-    public static function mlp($dataSet, $featureNum, $labelNum, $ignoreHeader = false)
+    public static function knn($dataSet, $featureNum, $ignoreHeader = false)
     {
         $dataSet = new CsvDataset($dataSet, $featureNum, $ignoreHeader);
 
-        $model = new Backpropagation(new MultilayerPerceptron([$featureNum, $featureNum, $labelNum]));
-        $model->train($dataSet->getSamples(), $dataSet->getTargets());
-        return $model;
+        $samples = $dataSet->getSamples();
+        $normalizer = new Normalizer();
+        $normalizer->fit($samples);
+        $normalizer->transform($samples);
+
+        $classifier = new KNearestNeighbors();
+        $classifier->train($samples, $dataSet->getTargets());
+
+        return $classifier;
     }
 }
