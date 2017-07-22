cda244462a35044c91fe6c61b9783374031cd6ad
Gogs <gogitservice@gmail.com>
2017-07-22 23:35:12 +0800

    complete file helper unit test

--- tests/components/helpers/FileHelperTest.php
+++ tests/components/helpers/FileHelperTest.php
@@ -13,4 +13,24 @@ class FileHelperTest extends BaseTestCase
         $extension = 'php';
         $this->assertEquals($extension, FileHelper::getExtensionName($file_path));
     }
+
+    public function testFileExists()
+    {
+        $this->assertTrue(FileHelper::fileExists(__FILE__));
+        $this->assertFalse(FileHelper::fileExists(__DIR__ . DIRECTORY_SEPARATOR . 'tmp'));
+    }
+
+    public function testDirExists()
+    {
+        $this->assertTrue(FileHelper::dirExists(__DIR__));
+        $this->assertFalse(FileHelper::dirExists(__DIR__ . DIRECTORY_SEPARATOR . 'tmp' . DIRECTORY_SEPARATOR));
+    }
+
+    public function testResourceExists()
+    {
+        $this->assertTrue(FileHelper::resourceExists(__FILE__));
+        $this->assertTrue(FileHelper::resourceExists(__DIR__));
+        $this->assertFalse(FileHelper::resourceExists(__DIR__ . DIRECTORY_SEPARATOR . 'tmp'));
+        $this->assertFalse(FileHelper::resourceExists(__DIR__ . DIRECTORY_SEPARATOR . 'tmp' . DIRECTORY_SEPARATOR));
+    }
 }
