1aaded40df8244a3a9a5da8e23ca89fa43ec8b69
Gogs <gogitservice@gmail.com>
2017-11-25 23:51:56 +0800

    refactor

--- components/traits/ArrayOp.php
+++ components/traits/ArrayOp.php
@@ -60,9 +60,10 @@ trait ArrayOp
 
     /**
      * String representation of object
-     * @link http://php.net/manual/en/serializable.serialize.php
+     *
+     * @link   http://php.net/manual/en/serializable.serialize.php
      * @return string the string representation of the object or null
-     * @since 5.1.0
+     * @since  5.1.0
      */
     public function serialize()
     {
@@ -71,12 +72,13 @@ trait ArrayOp
 
     /**
      * Constructs the object
-     * @link http://php.net/manual/en/serializable.unserialize.php
-     * @param string $serialized <p>
-     * The string representation of the object.
-     * </p>
+     *
+     * @link   http://php.net/manual/en/serializable.unserialize.php
+     * @param  string $serialized <p>
+     *                            The string representation of the object.
+     *                            </p>
      * @return void
-     * @since 5.1.0
+     * @since  5.1.0
      */
     public function unserialize($serialized)
     {
