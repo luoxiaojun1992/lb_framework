a574ba35a97ce6f6e2702950cacd94451e7d2a0b
Gogs <gogitservice@gmail.com>
2018-01-20 13:37:40 +0800

    bugfix

--- components/listeners/LogWriteListener.php
+++ components/listeners/LogWriteListener.php
@@ -5,9 +5,21 @@ namespace lb\components\listeners;
 use DebugBar\DataCollector\MessagesCollector;
 use lb\components\events\BaseEvent;
 use lb\components\traits\Singleton;
+use Monolog\Logger;
 
 class LogWriteListener extends BaseListener
 {
+    const LOG_LEVELS = [
+        Logger::DEBUG => 'debug',
+        Logger::INFO => 'info',
+        Logger::NOTICE => 'notice',
+        Logger::WARNING => 'warning',
+        Logger::ERROR => 'error',
+        Logger::CRITICAL => 'critical',
+        Logger::ALERT => 'alert',
+        Logger::EMERGENCY => 'emergency',
+    ];
+
     use Singleton;
 
     protected $context;
@@ -17,11 +29,11 @@ class LogWriteListener extends BaseListener
         parent::handler($event);
 
         /**
- * @var MessagesCollector $messageCollector 
-*/
+         * @var MessagesCollector $messageCollector
+         */
         $messageCollector = $event->getData();
 
         $logData = $event->getLogData();
-        $messageCollector->addMessage($logData['message'], $logData['level']);
+        $messageCollector->addMessage($logData['message'], self::LOG_LEVELS[$logData['level']] ?? 'info');
     }
 }
