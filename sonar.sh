1e821cce02600aa7bad6ef0e863d1bc4eae6b0a7
Gogs <gogitservice@gmail.com>
2018-01-22 16:32:27 +0800

    update sonar script

--- sonar.sh
+++ sonar.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
+mv ./vendor ../vendor-bak
 ~/sonar-scanner-3.0.3.778-macosx/bin/sonar-scanner   -Dsonar.projectKey=lb-framework   -Dsonar.sources=.   -Dsonar.host.url=http://localhost:9000   -Dsonar.login=5d5df6457a356c0b3dc62cb6cf664df6439c8945 -Dsonar.language=php
+mv ../vendor-bak ./vendor
