dcac9b2e593044fd986b18ad985bdb333baee3a4
Gogs <gogitservice@gmail.com>
2018-01-23 14:53:55 +0800

    sonar

--- sonar.sh
+++ sonar.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-#./vendor/bin/phpunit --coverage-xml ./reports ./tests
+./vendor/bin/phpunit --coverage-clover ./reports/phpunit.coverage.xml --log-junit ./reports/phpunit.report.xml ./tests
 
 mv ./vendor ../vendor-bak
-~/sonar-scanner-3.0.3.778-macosx/bin/sonar-scanner   -Dsonar.projectKey=lb-framework   -Dsonar.sources=.   -Dsonar.host.url=http://localhost:9000   -Dsonar.login=fe511242e2fb1be1f486b62de3ecc173847e684c -Dsonar.language=php
+~/sonar-scanner-3.0.3.778-macosx/bin/sonar-scanner   -Dsonar.projectKey=lb-framework   -Dsonar.sources=.   -Dsonar.host.url=http://localhost:9000   -Dsonar.login=fe511242e2fb1be1f486b62de3ecc173847e684c -Dsonar.language=php -Dsonar.php.coverage.reportPaths=./reports/phpunit.coverage.xml
 mv ../vendor-bak ./vendor
