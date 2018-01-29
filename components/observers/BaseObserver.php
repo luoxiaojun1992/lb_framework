a0fa54207ce06c1a16e436e47acbac366eaf2cd7
罗晓俊 <gogitservice@gmail.com>
2018-01-29 14:27:13 +0800

    bugfix

--- components/observers/BaseObserver.php
+++ components/observers/BaseObserver.php
@@ -22,26 +22,33 @@ class BaseObserver implements ObserverInterface
     {
         foreach(static::$event_listeners as $event_listener) {
             if ($event_listener[0] == $event_name) {
-                $event = $event ? : new BaseEvent();
+                $event = $event ?: new BaseEvent();
                 if ($data = $event_listener[2]) {
                     $event->setData($data);
                 }
 
                 /**
- * @var BaseListener $listener 
-*/
+                 * @var BaseListener $listener
+                 */
                 $listener = $event_listener[1];
-                if (!$ignoreQueue && $listener::$useQueue) {
-                    Lb::app()->queuePush(new Job(EventHandler::class, ['event_name' => $event_name, 'event' => $event]));
-                } else {
-                    if ($listener instanceof \Closure) {
-                        $callableListener = $listener;
-                    } else {
-                        $callableListener = [$listener, 'handler'];
-                    }
 
-                    call_user_func_array($callableListener, ['event' => $event]);
+                //Async Queue
+                if (!($listener instanceof \Closure)) {
+                    if (!$ignoreQueue && $listener::$useQueue) {
+                        Lb::app()->queuePush(new Job(EventHandler::class, ['event_name' => $event_name, 'event' => $event]));
+                        continue;
+                    }
                 }
+
+                //Sync Listener
+                if ($listener instanceof \Closure) {
+                    $callableListener = $listener;
+                } else {
+                    $callableListener = [$listener, 'handler'];
+                }
+
+                call_user_func_array($callableListener, ['event' => $event]);
+
             }
         }
     }
